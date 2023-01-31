070a74d27682c561f2c58eacbde68693b120894d
karimdaghari <karim_daghari@outlook.com>
2023-01-31 20:19:38 +0100

    feat: display 'to read' list

--- src/components/ToReadItem.tsx
+++ src/components/ToReadItem.tsx
@@ -1,3 +1,18 @@
-export function ToReadItem() {
-  return <div></div>;
+import { useSearchContext } from '~/contexts/search.context';
+import { Item, ItemProps } from './Item';
+
+export function ToReadItem(props: ItemProps) {
+  const { removeBookToRead } = useSearchContext();
+
+  return (
+    <Item
+      {...props}
+      actionButton={{
+        label: 'Remove',
+        onClick: () => {
+          removeBookToRead(props.id);
+        }
+      }}
+    />
+  );
 }
--- src/components/ToReadList.tsx
+++ src/components/ToReadList.tsx
@@ -1,3 +1,15 @@
+import { Paper } from '@mantine/core';
+import { useSearchContext } from '~/contexts/search.context';
+import { ToReadItem } from './ToReadItem';
+
 export function ToReadList() {
-  return <div></div>;
+  const { booksToRead } = useSearchContext();
+  return (
+    <Paper shadow='sm' p='md'>
+      {booksToRead.map(({ id, formats, title }) => {
+        const cover = formats['image/jpeg'];
+        return <ToReadItem id={id} title={title} cover={cover} />;
+      })}
+    </Paper>
+  );
 }
