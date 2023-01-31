24d8e119d42cce9c8d062972c9d390c68c5ba70b
karimdaghari <karim_daghari@outlook.com>
2023-01-31 18:45:54 +0100

    chore: add missing react-query devtool

--- src/providers/react-query.provider.tsx
+++ src/providers/react-query.provider.tsx
@@ -1,8 +1,14 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { IProvider } from '~/interfaces/provider';
 
 const client = new QueryClient();
 
 export function ReactQueryProvider({ children }: IProvider) {
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={client}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+    </QueryClientProvider>
+  );
 }
