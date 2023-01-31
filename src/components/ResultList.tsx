e45fcd59e142e06ce0b4e7934dd9745a52ab02e3
karimdaghari <karim_daghari@outlook.com>
2023-01-31 20:19:18 +0100

    feat: make result list scrollable

--- src/components/ResultList.tsx
+++ src/components/ResultList.tsx
@@ -1,11 +1,11 @@
-import { Paper } from '@mantine/core';
+import { Paper, ScrollArea } from '@mantine/core';
 import { useSearchContext } from '~/contexts/search.context';
 import { ResultItem } from './ResultItem';
 
 export function ResultList() {
   const { books, booksToReadIds } = useSearchContext();
   return (
-    <Paper shadow='sm' p='md'>
+    <Paper component={ScrollArea} h={750} shadow='sm' p='md'>
       {books.map(({ authors, id, title, formats }) => {
         const _authors = authors.map(({ name }) => name);
         const cover = formats['image/jpeg'];
