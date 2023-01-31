69b8de15e913341c60f31235802ae965f03a944f
karimdaghari <karim_daghari@outlook.com>
2023-01-31 18:49:13 +0100

    feat: add search functionality

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,3 +1,27 @@
+import { Button, Flex, TextInput } from '@mantine/core';
+import { useState } from 'react';
+import { useSearchContext } from '~/contexts/search.context';
+
 export function SearchBar() {
-  return <div></div>;
+  const { setQuery } = useSearchContext();
+  const [value, setValue] = useState('');
+  return (
+    <Flex gap='sm'>
+      <TextInput
+        placeholder='Search for books...'
+        value={value}
+        onChange={({ currentTarget: { value } }) => setValue(value)}
+        style={{
+          width: '100%'
+        }}
+      />
+      <Button
+        onClick={() => {
+          setQuery(value);
+        }}
+        disabled={!value}>
+        Search
+      </Button>
+    </Flex>
+  );
 }
--- src/providers/search.provider.tsx
+++ src/providers/search.provider.tsx
@@ -1,12 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import { useState } from 'react';
 import { SearchContext } from '~/contexts/search.context';
+import { IApiResponse } from '~/interfaces/api';
 import { IProvider } from '~/interfaces/provider';
 
 export function SearchProvider({ children }: IProvider) {
   const [query, setQuery] = useState('');
+  const { data, error } = useQuery({
+    queryKey: ['search', query],
+    enabled: query !== '',
+    queryFn: async () => {
+      const { data } = await axios.get<IApiResponse>(
+        'https://gutendex.com/books',
+        {
+          params: {
+            search: query
+          }
+        }
+      );
+      return data;
+    }
+  });
 
   return (
-    <SearchContext.Provider value={{ query, setQuery }}>
+    <SearchContext.Provider value={{ setQuery }}>
       {children}
     </SearchContext.Provider>
   );
