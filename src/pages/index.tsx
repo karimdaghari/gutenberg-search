cd147c3174c448dcf616f6de042f031cb4976a85
karimdaghari <karim_daghari@outlook.com>
2023-02-01 01:34:03 +0100

    add link to github

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,50 @@
-import { AppShell, Container, SimpleGrid, Stack } from '@mantine/core';
+import {
+  Anchor,
+  AppShell,
+  Center,
+  Container,
+  SimpleGrid,
+  Stack
+} from '@mantine/core';
 import { ResultList } from '~/components/ResultList';
 import { SearchBar } from '~/components/SearchBar';
 import { ToReadList } from '~/components/ToReadList';
 
 export default function Index() {
   return (
-    <AppShell>
-      <Container>
-        <Stack>
-          <SearchBar />
-          <SimpleGrid
-            breakpoints={[
-              {
-                minWidth: 'sm',
-                cols: 1
-              },
-              {
-                minWidth: 'md',
-                cols: 2
-              }
-            ]}>
-            <ResultList />
-            <ToReadList />
-          </SimpleGrid>
-        </Stack>
-      </Container>
-    </AppShell>
+    <>
+      <Center bg='dark' py='xs'>
+        <Anchor
+          style={{
+            color: 'white'
+          }}
+          href='https://github.com/karimdaghari/gutenberg-search'
+          target='_blank'
+          align='center'>
+          See the source on GitHub
+        </Anchor>
+      </Center>
+      <AppShell>
+        <Container>
+          <Stack>
+            <SearchBar />
+            <SimpleGrid
+              breakpoints={[
+                {
+                  minWidth: 'sm',
+                  cols: 1
+                },
+                {
+                  minWidth: 'md',
+                  cols: 2
+                }
+              ]}>
+              <ResultList />
+              <ToReadList />
+            </SimpleGrid>
+          </Stack>
+        </Container>
+      </AppShell>
+    </>
   );
 }
