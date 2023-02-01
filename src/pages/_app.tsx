ac7d02db07c2b66b3997f71633e00cd0ee33929f
karimdaghari <karim_daghari@outlook.com>
2023-02-01 01:35:05 +0100

    chore: rename project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App(props: AppProps) {
   return (
     <>
       <Head>
-        <title>CH-Challenge</title>
+        <title>Gutenberg Search | Karim Daghari</title>
         <meta
           name='viewport'
           content='minimum-scale=1, initial-scale=1, width=device-width'
